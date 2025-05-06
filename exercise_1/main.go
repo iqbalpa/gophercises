9b702c5585157d4ae239df11ccf779d65642a81a
iqbalpa <iqbalpahlevi07@gmail.com>
2025-05-06 10:41:17 +0700

    e1: refactor code

--- exercise_1/main.go
+++ exercise_1/main.go
@@ -1,79 +1,70 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
-	"main/utils"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 )
 
-const (
-	csvVar = "csv"
-	limitVar = "limit"
-)
-
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-
-	args := os.Args[1:]
-	kv := utils.ArgParser(args)
+	csv := flag.String("csv", "problems.csv", "File path to problem (default: problems.csv)")
+	limit := flag.Int("limit", 30, "Quiz time limit (default: 30s)")
+	flag.Parse()
 	
-	limit := 30
-	if kv[limitVar] != "" {
-		limit, _ = strconv.Atoi(kv[limitVar])
-	}
-	csvPath := kv[csvVar]
-	if csvPath == "" {
-		csvPath = "./problems.csv"
-	}
-
 	counter := 0
-	csvRecords := readCsvFile(csvPath)
+	problems := readCsvFile(*csv)
+	timer := time.NewTimer(time.Duration(*limit) * time.Second)
+	
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = ", (i+1), p.Q)
+		
+		answerChan := make(chan string)
+		go func(){
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerChan <- answer
+		}()
 
-	// init timer
-	timer := time.NewTimer(time.Duration(limit) * time.Second)
-
-	loop: for i, rec := range csvRecords {
 		select {
 			case <- timer.C:
-				break loop
-			default:
-				q, ca := rec[0], rec[1]
-				fmt.Printf("Problem #%d: %s = ", (i+1), q)
-				a, _ := reader.ReadString('\n')
-				a = strings.Trim(a, "\n")
-
-				cai, _ := strconv.Atoi(ca)
-				ai, _ := strconv.Atoi(a)
-				if cai == ai {
-					counter += 1
-				} 
+				fmt.Println("\nTimed out!")
+				return
+			case answer := <- answerChan:
+				if answer == p.A {
+					counter++
+					fmt.Println("Correct!")
+				}
 		}
 	}
 
-	fmt.Printf("You scored %d out of %d\n", counter, len(csvRecords))
+	fmt.Printf("You scored %d out of %d\n", counter, len(problems))
 }
 
-
-// helper fucntion
-func readCsvFile(filePath string) [][]string {
-	if filePath == "" {
-		filePath = "./problems.csv"
-	}
-	f, err := os.Open(filePath)
+func readCsvFile(fpath string) []Problem {
+	f, err := os.Open(fpath)
 	if err != nil {
-			log.Fatal("Unable to read input file " + filePath, err)
+		log.Fatal("Unable to read input file " + fpath, err)
 	}
 	defer f.Close()
 	csvReader := csv.NewReader(f)
 	records, err := csvReader.ReadAll()
 	if err != nil {
-			log.Fatal("Unable to parse file as CSV for " + filePath, err)
+		log.Fatal("Unable to parse file as CSV for " + fpath, err)
 	}
-	return records
+	ret := make([]Problem, len(records))
+	for i,rec := range records {
+		ret[i] = Problem{
+			Q: rec[0],
+			A: rec[1],
+		}
+	}
+	return ret
+}
+
+type Problem struct {
+	Q string
+	A string
 }
